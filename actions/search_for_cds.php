c9ecae151a17574d3a768256ae96c593db5c5e9b
Nikez44 <nike-o_94@hotmail.com>
2015-05-06 16:00:37 -0500

    ya funciona cuando no hay selects

--- actions/search_for_cds.php
+++ actions/search_for_cds.php
@@ -17,9 +17,10 @@ $artist = $_GET["artist"];
 $category = $_GET["category"];
 
 if ($cd_name == 0 || $artist == 0 || $category == 0) {
-    $sentencia_sql = "SELECT disco.*, interprete.*, categoria.desc_cat, sello.desc_s,
- disco.precio_d FROM (disco INNER JOIN sello INNER JOIN categoria INNER JOIN interprete)
- WHERE (disco.cod_d = interprete.cod_i AND disco.cod_s = sello.cod_S) AND  disco.nom_d LIKE '%" . $key_word . "%'";
+    $sentencia_sql = "SELECT DISTINCT disco.*, interprete.*, categoria.*, sello.desc_s, disco.precio_d
+                      FROM (disco INNER JOIN sello INNER JOIN categoria INNER JOIN interprete)
+                      WHERE (disco.cod_i = interprete.cod_i AND disco.cod_s = sello.cod_S AND disco.cod_cat = categoria.cod_cat)
+                      AND (disco.nom_d LIKE '%" . $key_word . "%' OR interprete.desc_i LIKE '%" . $key_word . "%' OR categoria.desc_cat LIKE '%" . $key_word . "%') ";
 } else {
     $sentencia_sql = "SELECT disco.*, interprete.*, categoria.desc_cat, sello.desc_s,
  disco.precio_d FROM (disco INNER JOIN sello INNER JOIN categoria INNER JOIN interprete)
@@ -27,7 +28,7 @@ if ($cd_name == 0 || $artist == 0 || $category == 0) {
   AND categoria.cod_cat=$category AND interprete.cod_i=$artist";
 }
 
-echo $sentencia_sql;
+//echo $sentencia_sql;
 //exit;
 
 $resultados = ejecutar_query($sentencia_sql);
