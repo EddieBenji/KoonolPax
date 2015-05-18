5bb36be0adb3f96dcf37d40abec472005400cdf2
Lalo <eduardo_benji@hotmail.com>
2015-05-18 17:03:32 -0500

    Usuarios activos

--- actions/usuarios_activos.php
+++ actions/usuarios_activos.php
@@ -9,6 +9,6 @@
 //incluye el dao con la información de la BD:
 include 'DB/global.php';
 
-$sentencia_sql = "SELECT *, count(*) AS pedidos_realizados FROM pedido_m JOIN usuario ON pedido_m.cod_u=usuario.cod_u GROUP BY pedido_m.cod_u ORDER BY pedidos_realizados LIMIT 5";
+$sentencia_sql = "SELECT *, count(*) AS pedidos_realizados FROM pedido_m JOIN usuario ON pedido_m.cod_u=usuario.cod_u GROUP BY pedido_m.cod_u ORDER BY pedidos_realizados DESC LIMIT 5";
 
 $usuarios_activos = ejecutar_query($sentencia_sql);
