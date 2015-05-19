aa3997d30bc9c26c696cc91da406ee83e907405c
Nikez44 <nike-o_94@hotmail.com>
2015-05-19 14:37:09 -0500

    Merge branch 'romario-bueno' of https://github.com/EddieBenji/KoonolPax into Romario
    Conflicts:
    	actions/disco/alta_disco.php

--- _menu.php
+++ _menu.php
@@ -4,67 +4,33 @@
 <table width="1200px;">
     <thead>
     <tr>
-        <td>
-            Alta
-        </td>
-        <td>
-            Baja
-        </td>
-        <td>
-            Modificar
-        </td>
-        <td>
-            Ventas del Mes
-        </td>
+        <td>Alta</td>
+        <td>Baja</td>
+        <td>Modificar</td>
+        <td>Ventas del Mes</td>
     </tr>
     </thead>
     <tbody>
     <tr>
-        <td>
-            <a href="agregar_disco.php">Discos</a>
-        </td>
-        <td>
-            <a href="baja_disco.php">Discos</a>
-        </td>
-        <td>
-            <a href="modificardisco.php">Discos</a>
-        </td>
-        <td>
-            <a href="ver_usuarios_activos.php">Usuarios mas activos</a>
-        </td>
+        <td><a href="agregar_disco.php">Discos</a></td>
+        <td><a href="baja_disco.php">Discos</a></td>
+        <td><a href="modificardisco.php">Discos</a></td>
+        <td><a href="ver_usuarios_activos.php">Usuarios mas activos</a></td>
     </tr>
     <tr>
-        <td>
-            <a href="altainterprete.php">Intérprete</a>
-        </td>
-        <td>
-            <a href="bajainterprete.php">Intérprete</a>
-        </td>
-        <td>
-            <a href="modificarinterprete.php">Intérprete</a>
-        </td>
+        <td><a href="altainterprete.php">Intérprete</a></td>
+        <td><a href="bajainterprete.php">Intérprete</a></td>
+        <td><a href="modificarinterprete.php">Intérprete</a></td>
     </tr>
     <tr>
-        <td>
-            <a href="altacategoria.php">Categorías</a>
-        </td>
-        <td>
-            <a href="bajacategoria.php">Categorías</a>
-        </td>
-        <td>
-            <a href="modificarcategoria.php">Categorías</a>
-        </td>
+        <td><a href="altacategoria.php">Categorías</a></td>
+        <td><a href="bajacategoria.php">Categorías</a></td>
+        <td><a href="modificarcategoria.php">Categorías</a></td>
     </tr>
     <tr>
-        <td>
-            <a href="altasello.php">Sellos</a>
-        </td>
-        <td>
-            <a href="bajasello.php">Sellos</a>
-        </td>
-        <td>
-            <a href="modificarsello.php">Sellos</a>
-        </td>
+        <td><a href="altasello.php">Sellos</a></td>
+        <td><a href="bajasello.php">Sellos</a></td>
+        <td><a href="modificarsello.php">Sellos</a></td>
     </tr>
     </tbody>
 </table>
