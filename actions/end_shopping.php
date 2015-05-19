408165d5ebe057fa5c82396895a232b007dbcfdb
Lalo <eduardo_benji@hotmail.com>
2015-05-19 15:59:35 -0500

    Quitanto lo innecesario de agregar pedidos

--- actions/end_shopping.php
+++ actions/end_shopping.php
@@ -14,14 +14,14 @@ $queryPedidoM = "INSERT INTO pedido_m(cod_u, fec_p, fin_p) VALUES($user_id, '$da
 
 ejecutar_query($queryPedidoM);
 
-$queryBuscarPedidoM = "SELECT * FROM pedido_m where fec_p='$date'";
-
-$pedido = ejecutar_query($queryBuscarPedidoM);
-$row = $pedido->fetch_assoc();
-$ped_id = $row["cod_p"];
-
-$queryPedidoD = "INSERT INTO pedido_d(cod_p, cod_d, cant_d) VALUES($ped_id, $disc_id, $disc_qty)";
-ejecutar_query($queryPedidoD);
+//$queryBuscarPedidoM = "SELECT * FROM pedido_m where fec_p='$date'";
+//
+//$pedido = ejecutar_query($queryBuscarPedidoM);
+//$row = $pedido->fetch_assoc();
+//$ped_id = $row["cod_p"];
+//
+//$queryPedidoD = "INSERT INTO pedido_d(cod_p, cod_d, cant_d) VALUES($ped_id, $disc_id, $disc_qty)";
+//ejecutar_query($queryPedidoD);
 
 
 header('location: ../index.php');
